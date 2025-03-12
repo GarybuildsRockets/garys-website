08ef6d653309302fb2d420c7bb0235056adee49b
GarybuildsRockets <gkany@illinois.edu>
2025-03-11 22:38:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -97,18 +97,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("Failed to send message. Please try again.");
             });
     });
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        if (target) {
-            window.scrollTo({
-                top: target.offsetTop,
-                behavior: 'smooth'
-            });
-        }
-    });
-});
 
 
 
